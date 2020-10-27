21be9c4b4aacd5f511bdd76ecf825f51c0e6fabb
Roman <first.luker@gmail.com>
2020-10-27 08:47:10 +0200

    Refactored

--- BitsOrchestraTest/Service/DataValidation.cs
+++ BitsOrchestraTest/Service/DataValidation.cs
@@ -6,27 +6,23 @@ namespace BitsOrchestraTest.Service
     {
         public bool StringToInt(string value) 
         {
-            int intValue;
-            return int.TryParse(value, out intValue);
+            return int.TryParse(value, out _);
         }
 
         public bool StringToDateTime(string value)
         {
-            DateTime dateTime;
-            return DateTime.TryParse(value, out dateTime);
+            return DateTime.TryParse(value, out _);
             
         }
 
         public bool StringToDecimal(string value)
         {
-            decimal decimalValue;
-            return decimal.TryParse(value, out decimalValue);
+            return decimal.TryParse(value, out _);
         }
 
         public bool StringToBoolean(string value)
         {
-            bool boolValue;
-            return bool.TryParse(value, out boolValue);
+            return bool.TryParse(value, out _);
         }
     }
 }
